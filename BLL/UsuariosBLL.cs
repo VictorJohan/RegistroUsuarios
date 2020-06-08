f2c28a495b1b4d9b6ab426e3bc18949a4eec4c08
Victor Johan <Vicjohan23@gmail.com>
2020-06-07 22:23:48 -0400

    Add project files.

---
+++ BLL/UsuariosBLL.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using RegistroUsuario.DAL;
+using RegistroUsuario.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace RegistroUsuario.BLL
+{
+    class UsuariosBLL
+    {
+        public static bool Guardar(Usuarios usuario)
+        {
+            if (!Existe(usuario.IdUsuario))
+            {
+                return Insertar(usuario);
+            }
+            else
+            {
+                return Modificar(usuario);
+            }
+        }
+
+        public static bool Existe(string id)
+        {
+            bool ok = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                ok = contexto.Usuarios.Any(e => e.IdUsuario == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Insertar(Usuarios usuario)
+        {
+            bool ok = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Usuarios.Add(usuario);
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Modificar(Usuarios usuario)
+        {
+            bool ok = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(usuario).State = EntityState.Modified;
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return ok;
+        }
+
+        public static Usuarios Buscar(string id)
+        {
+            Usuarios usuario;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                usuario = contexto.Usuarios.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return usuario;
+        }
+
+        public static bool Eliminar(string id)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                var usuario = contexto.Usuarios.Find(id);
+                if (usuario != null)
+                {
+                    contexto.Usuarios.Remove(usuario);
+                    ok = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+    }
+}
---
+++ Migrations/20200608012851_Migracion_Inicial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroUsuario.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<string>(nullable: false),
+                    Clave = table.Column<string>(nullable: true),
+                    Nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.IdUsuario);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
